77199f8d96851fb82251f6e1e1ea155e8de56703
Karthik K <karthiksuresh322004@gmail.com>
2026-01-18 19:03:41 +0530

    play.py

--- play.py
+++ play.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 
 class DQN(nn.Module):
-    def __init__(self, obs_dim: int, n_actions: int, hidden=256):
+    def __init__(self, obs_dim: int, n_actions: int, hidden=128):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(obs_dim, hidden),
@@ -28,7 +28,7 @@ def flatten_obs(obs: np.ndarray) -> np.ndarray:
 def main():
     size = 30
     model_file = "fast_dqn_dynamic_30.pt"
-    max_steps = size * size * 2
+    max_steps = 600
 
     env = gym.make("gymnasium_env/GridWorld-v0", size=size, render_mode="human", max_steps=max_steps)
 
@@ -36,7 +36,7 @@ def main():
     obs_dim = obs0.size
     n_actions = env.action_space.n
 
-    model = DQN(obs_dim, n_actions, hidden=256)
+    model = DQN(obs_dim, n_actions, hidden=128)
     model.load_state_dict(torch.load(model_file, map_location="cpu"))
     model.eval()
 
